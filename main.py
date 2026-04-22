0acda0fc2c2332bb7341ae565eea296dd17fbc40
harsha-tech006 <hv012609@gmail.com>
2026-04-22 11:09:23 +0530

    hmil

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("Home.HTML")
+
+@app.route("/about")
+def about():
+    return render_template("About.HTML")
+
+@app.route("/csignup")
+def signup():
+    return render_template("signup.HTML")
+
+@app.route("/login")
+def login():
+    return render_template("LOGIN.HTML")
+
+if __name__ == "__main__":
+    app.run(debug=True)
